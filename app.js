b8235ad387cce60d7afa01cc8cda90cde590cc12
kisakyegordon <gordon56692@gmail.com>
2018-06-19 15:26:58 +0100

    modify app file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require ('http');
 const path = require('path');
 
 let app = express();
-app.use( express.static(path.join(__dirname, './build')) );
+app.use( express.static(path.join(__dirname, './public')) );
 // app.use('/', express.static('./public'));
 
 const port = process.env.PORT || '8080';
